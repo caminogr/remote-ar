bb11eac819fb8f7c2f5cb42d6b867e06b44512be
kaminora <kaminoura9207@gmail.com>
2020-03-28 22:37:07 +0900

    Integrate sentry

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 import {router} from './router';
+
+  Sentry.init({
+    dsn: 'https://e88ac66c645941849e0e83a3121245c5@sentry.io/5178328',
+    integrations: [
+      new Integrations.Vue({
+        Vue, attachProps: true
+      }),
+      new Integrations.CaptureConsole({
+        levels: ['error']
+      })
+    ],
+  });
 
 Vue.config.productionTip = false
 
