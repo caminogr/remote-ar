8ac0431cad98d2e2d440dd33d85265b42bc39b81
kaminora <kaminoura9207@gmail.com>
2020-03-28 18:26:59 +0900

    Get current location in Home component

---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <button @click="getCurrentPosition">
+        get your current position
+      </button>
+      <p>lat: {{ currentLocation.latitude }}</p>
+      <p>lng: {{ currentLocation.longitude }}</p>
+    </div>
+    <router-link to="ar">AR</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data: function() {
+    return {
+      currentLocation: { latitude: null, longitude: null }
+    };
+  },
+  methods: {
+    getCurrentPosition: function() {
+      navigator.geolocation.getCurrentPosition(
+        function(pos) {
+          const { latitude, longitude } = pos.coords;
+          this.currentLocation.latitude = latitude;
+          this.currentLocation.longitude = longitude;
+        }.bind(this),
+        function(err) {
+          console.log("err: ", err);
+        }
+      );
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from '@/components/Home'
 import AR from '@/components/AR'
 
 Vue.use(Router)
@@ -8,6 +9,7 @@ Vue.use(Router)
 export const router = new Router({
   mode: 'history',
   routes: [
+    { path: '/', component: Home },
     { path: '/ar', component: AR },
   ]
 })
